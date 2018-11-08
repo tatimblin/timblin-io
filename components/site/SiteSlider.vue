e7783fb24375c83547a519bff94f4198700be6bf
Tristan Timblin <tatimblin@gmail.com>
2018-11-08 01:01:24 -0500

    group slide

--- components/site/SiteSlider.vue
+++ components/site/SiteSlider.vue
@@ -2,26 +2,36 @@
     <section class="slider large-width">
         <div class="row">
             <div class="slider-view">
+                <transition-group 
+                    name="list" 
+                    tag="div"
+                    @enter="enter"
+                    @leave="leave"
+                >
                 <div 
-                    v-for="item in selectItems"
-                    :key="item.index"
+                    v-for="(slide, index) in slides[selectedGroup]"
+                    :key="index"
                     class="slider-view-cell"
                 >
-                    {{ item.title }}
+                   {{slide}}
                 </div>
+                </transition-group>
             </div>
             <div class="slider-nav">
                 <div class="slider-nav-ctrl">
-                    <div class="slider-nav-ctrl-prev" role="img" alt="previous"></div>
-                    <div class="slider-nav-ctrl-next" role="img" alt="next" @click="slide()"></div>
+                    <div class="slider-nav-ctrl-prev" role="img" alt="previous" @click="currentSlide('prev')"></div>
+                    <div class="slider-nav-ctrl-next" role="img" alt="next" @click="currentSlide('next')"></div>
                 </div>
-                <div class="slider-nav-prog"></div>
+                <div class="slider-nav-prog">
+                    <div class="slider-nav-prog-bar" :style="{ width: timer + '%'}"></div>    
+                </div>
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import TweenMax from 'gsap';
 
 export default {
   components: {
@@ -36,30 +46,68 @@ export default {
     }));
     return {
       items,
-      start: 0,
-      end: 4,
+      slides: [],
+      selected: 0,
+      timer: 0,
     }
   },
-  methods: {
-      slide() {
-          if (this.start >= this.items.length - 4) {
-              this.start = 0
-          } else {
-              this.start = this.start + 4
-          }
-          if (this.end >= this.items.length) {
-              this.end = 4
-          } else {
-              this.end = this.end + 4
-          }
-          console.log0
+  created() {
+      const count = 4;
+      const titles = this.items.map(a => a.title);
+      var newArray = [];
+      while (titles.length > 0) {
+        newArray.push(titles.splice(0, count)); 
       }
+      console.log(newArray);
+      this.slides = newArray;
+  },
+  methods: {
+      currentSlide(dir) {
+          const l = this.slides.length - 1;
+          if (dir === 'next' && this.selected === l) {
+              this.selected = 0;
+          } else if (dir === 'next' ) {
+              ++this.selected;
+              console.log('yes');
+          } else if (dir === 'prev' && this.selected === 0) {
+              this.selected = l - 1;
+          } else if (dir === 'prev') {
+              --this.selected;
+          }
+          console.log(this.selected + ' | ' + l)
+      },
+      enter (el, done) {
+          TweenMax.fromTo(el, 3, {
+              x: '-200px',
+          },
+          {
+              x: '0',
+              onComplete: done,
+          });
+      },
+      leave (el, done) {
+          TweenMax.to(el, 3, {
+              x: '200px',
+              onComplete: done,
+          });
+      },
   },
   computed: {
-      selectItems: function () {
-          return this.items.slice(this.start, this.end)
-      }
-  }
+      selectedGroup: function () {
+          return this.selected;
+      },
+      percentComplete: function () {
+          return this.timer.toFixed(0);
+      },
+  },
+  watch: {
+      duration: function(newValue, done) {
+          TweenMax.to(this.$data, 3, {timer: newValue}, {onComplete: this.slide});
+      },
+      delay: function(newValue, done) {
+          TweenMax.to(this.$data, 1, {timer: newValue}, {onComplete: this.slide});
+      },
+  },
 }
 </script>
 
@@ -67,7 +115,7 @@ export default {
 @import '~assets/sass/utilities/_variables.scss';
 
 .slider {
-    &-view {
+    &-view div {
         display: grid;
         grid-column-gap: $spacing;
         grid-template-columns: repeat(auto-fill, minmax(calc(25% - 30px), 1fr));
@@ -85,7 +133,7 @@ export default {
             grid-row: 1/1;
             grid-column: 1/1;
         }
-        &-cell {
+        .slider-view-cell {
             background-color: orange;
         }
     }
@@ -106,12 +154,11 @@ export default {
                 background-color: lighten(#2b2b2b, 65%);
                 z-index: 99;
             }
-            &:after {
+            &-bar {
                 content: '';
                 position: absolute;
-                width: 25%; height: 1px;
+                height: 1px;
                 background-color: lighten(#2b2b2b, 0%);
-                box-shadow: 0px 0.1px 0.1px 0.2px #2b2b2b;
                 z-index: 100;
             }
         }
@@ -126,6 +173,12 @@ export default {
                 margin-left: $spacing/2;
                 background-image: url('~/assets/ui/arrow.svg');
                 background-repeat: no-repeat;
+                cursor: pointer;
+                transition: opacity 300ms $ease;
+
+                &:hover {
+                    opacity: 0.66;
+                }
             }
             &-prev {
                 transform: rotate(180deg);
