650628e9ff46dff47536dd3f6a0cf51dd3ad1d26
Tristan Timblin <tatimblin@gmail.com>
2019-02-20 23:40:48 -0500

    very basic canvas element

--- pages/labs/middle-child.vue
+++ pages/labs/middle-child.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="page">
+  <section class="page" ref="page">
     <div class="overlay">
         <div class="overlay__name">
             <h2>J.Cole</h2>
@@ -11,7 +11,7 @@
             parental advisory
         </div>
     </div>
-    <canvas id="canvas"></canvas>
+    <canvas ref="static" id="canvas" width="100%" height="100%"></canvas>
   </section>
 </template>
 
@@ -20,9 +20,44 @@
 export default {
     data() {
         return {
-            title: 'Issa demo'
+            toggle: true,
         }
-    }
+    },
+    mounted() {
+        this.noise(this.$refs.static)
+
+        function loop() {
+            this.toggle = !this.toggle
+            if (this.toggle) {
+                setTimeout( function()  {
+                    requestAnimationFrame(loop)
+                }, 20)
+                return
+            }
+            this.noise(this.$refs.static)
+            requestAnimationFrame(loop)
+        };
+    },
+    methods: {
+        noise(canvasElement) {
+            console.log(canvasElement);
+            const ctx = canvasElement.getContext('2d');
+            ctx.clearRect(0, 0, 300, 150);
+            ctx.fillStyle = 'red';
+            ctx.font = '20px Georgia';
+            ctx.fillText('hello', 10, 50);
+            let w = ctx.canvas.width,
+                h = ctx.canvas.height,
+                imageData = ctx.createImageData(w, h),
+                l = imageData.data.length,
+                i = 0;
+
+            for(; i < l;)
+                imageData.data[i++] = (155 * Math.random()) + 70 | 20;
+
+            ctx.putImageData(imageData, 0, 0);
+        },
+    },
 }
 </script>
 
